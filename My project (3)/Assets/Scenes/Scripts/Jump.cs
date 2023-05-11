fcceb0fc1d152ec5f067f3f73e75862bd6bc870e
qizheng0815 <qizheng15@hotmail.com>
2023-05-11 01:36:45 -0400

    Tile Palette + Lube + fix jumpin later

--- My project (3)/Assets/Scenes/Scripts/Jump.cs
+++ My project (3)/Assets/Scenes/Scripts/Jump.cs
@@ -14,14 +14,20 @@ public class Jump : MonoBehaviour
     bool grounded;
     Vector2 playerSize;
 
+    [SerializeField] bool groundedMid;
+    [SerializeField] bool groundedRight;
+    [SerializeField] bool groundedLeft;
+
+
+
     void Awake()
     {
-        playerSize = GetComponent<BoxCollider2D>().size;    
+        playerSize = GetComponent<BoxCollider2D>().size;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)&&grounded)
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             jumpRequest = true;
         }
@@ -29,7 +35,7 @@ public class Jump : MonoBehaviour
 
     void FixedUpdate()
     {
-        
+
         if (jumpRequest)
         {
             //GetComponent<Rigidbody2D>().velocity += Vector2.up * jumpVelocity;
@@ -38,22 +44,22 @@ public class Jump : MonoBehaviour
             jumpRequest = false;
             grounded = false;
         }
-        else 
-        
+        else
+
         {
-            
-            Vector2 Right = new Vector2(0.8f,0f);
-            Vector2 Left = new Vector2(-0.8f,0f); 
-            
+
+            Vector2 Right = new Vector2(0.8f, 0f);
+            Vector2 Left = new Vector2(-0.8f, 0f);
+
             Vector2 rayStart = (Vector2)transform.position + Vector2.down * playerSize.y * 0.5f;
-            Vector2 rayRight = ((Vector2)transform.position+Right) + Vector2.down * playerSize.y * 0.5f;
-            Vector2 rayLeft = ((Vector2)transform.position+Left) + Vector2.down * playerSize.y * 0.5f;
-         
-            bool groundedMid = Physics2D.Raycast(rayStart, Vector2.down, groundedSkin, mask);
-            bool groundedRight = Physics2D.Raycast(rayRight, Vector2.down, groundedSkin, mask);
-            bool groundedLeft = Physics2D.Raycast(rayLeft, Vector2.down, groundedSkin, mask);
-            
-            if ((groundedMid == true) || (groundedRight == true) || (groundedLeft == true)) 
+            Vector2 rayRight = ((Vector2)transform.position + Right) + Vector2.down * playerSize.y * 0.5f;
+            Vector2 rayLeft = ((Vector2)transform.position + Left) + Vector2.down * playerSize.y * 0.5f;
+
+            groundedMid = Physics2D.Raycast(rayStart, Vector2.down, groundedSkin, mask);
+            groundedRight = Physics2D.Raycast(rayRight, Vector2.down, groundedSkin, mask);
+            groundedLeft = Physics2D.Raycast(rayLeft, Vector2.down, groundedSkin, mask);
+
+            if ((groundedMid == true) || (groundedRight == true) || (groundedLeft == true))
             {
                 grounded = true;
             }
